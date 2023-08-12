86c1356d7a63c2c675ff839702be94b54015a1ad
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-13 01:31:09 +0300

    get acces to the DAO

--- app/src/main/java/com/omrilhn/noteapp/di/AppModule.kt
+++ app/src/main/java/com/omrilhn/noteapp/di/AppModule.kt
@@ -1,10 +1,18 @@
 package com.omrilhn.noteapp.di
 
+import com.omrilhn.noteapp.data.NoteDatabase
+import com.omrilhn.noteapp.data.NoteDatabaseDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class) // We want to create it into different instances
 @Module
 object AppModule {
+    @Singleton
+    @Provides
+    fun provideNotesDao(noteDatabase: NoteDatabase):NoteDatabaseDao
+    = noteDatabase.noteDao() // get acces to the DAO
 }
