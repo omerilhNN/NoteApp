926cb4053f8140f7a72640c101cf851589661b1c
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-13 01:13:35 +0300

    FlowState added getNotes function

--- app/src/main/java/com/omrilhn/noteapp/data/NoteDatabase.kt
+++ app/src/main/java/com/omrilhn/noteapp/data/NoteDatabase.kt
@@ -6,8 +6,8 @@ import com.omrilhn.noteapp.model.Note
 
 @Database(entities = [Note::class],version = 1, exportSchema = false)
 abstract  class NoteDatabase: RoomDatabase(){
-    abstract fun noteDao():NoteDatabaseDao{ //DAO: Database access object
+    abstract fun noteDao():NoteDatabaseDao //DAO: Database access object
 
-    }
+
 
 }
--- app/src/main/java/com/omrilhn/noteapp/data/NoteDatabaseDao.kt
+++ app/src/main/java/com/omrilhn/noteapp/data/NoteDatabaseDao.kt
@@ -1,12 +1,34 @@
 package com.omrilhn.noteapp.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.omrilhn.noteapp.model.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDatabaseDao {
 
-    @Query("SELECT * from notes_tbl")
-    fun getNotes(): List<Note> //Go SELECT ALL FROM NOTES_TBL AND RETURN LIST OF NOTE 
+    @Query("SELECT * from notes_tbl")//Go SELECT ALL FROM NOTES_TBL AND RETURN LIST OF NOTE
+    fun getNotes(): Flow<List<Note>> //Asyncronious - MutableState but with more options
+    @Query ("SELECT * from notes_tbl where id=:id") //Query where id equals the id that we are gonna pass
+    suspend fun getNoteById(id:String) : Note
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE) //When there is an error
+    suspend fun insert(note:Note)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(note:Note)
+
+    //If we want to delete everything
+    @Query("DELETE from notes_tbl")
+    suspend fun deleteAll()
+
+    //Delete one item
+    @Delete
+    suspend fun deleteNote(note:Note)
+
 }
